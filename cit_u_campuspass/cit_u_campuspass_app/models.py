cd566c92085d33bde92912cdb9821f45ba78097e
joshnuevas <joshnuevas3065@gmail.com>
2025-10-04 23:08:36 +0800

    feat(campuspass): complete UI for register, login, and dashboard; implement basic backend

--- cit_u_campuspass/cit_u_campuspass_app/models.py
+++ cit_u_campuspass/cit_u_campuspass_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    phone = models.CharField(max_length=15, blank=True)
+
+class Visit(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    code = models.CharField(max_length=20)
+    title = models.CharField(max_length=100)
+    date_start = models.DateTimeField()
+    date_end = models.DateTimeField()
+    status_choices = [
+        ('Active', 'Active'),
+        ('Upcoming', 'Upcoming'),
+        ('Expired', 'Expired'),
+    ]
+    status = models.CharField(max_length=10, choices=status_choices, default='Upcoming')
+
+    def __str__(self):
+        return f"{self.code} - {self.user.username}"
